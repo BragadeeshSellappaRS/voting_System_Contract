4516fa4e757b6b674d7ad1dc0f0f4143a7fad315
BragadeeshSellappaRS <117265751+BragadeeshSellappaRS@users.noreply.github.com>
2024-05-03 15:15:38 +0530

    up/

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -4,23 +4,18 @@ pragma solidity ^0.8.0;
 contract VotingSystem{
     string public P1 = "Party A";
     string public P2 = "Party B";
-    uint voteA;
-    uint voteB;
-    
-
-    event cast(address voter,string party);
-    event winner(string win_party,uint win_votes);
-    
-
+    uint private voteA;
+    uint private voteB;
+     mapping(address => bool) public voted;
 
     function vote_Party_A() public{
-        
+        require(!voted[msg.sender],"Already voted");
+        voted[msg.sender] = true;
         voteA +=1;
-     
-        
-    
     }
     function vote_Party_B() public{
+        require(!voted[msg.sender],"Already votd");
+        voted[msg.sender] = true;
         voteB +=1;
     }
     function DeclareWinner() public view returns(string memory){
